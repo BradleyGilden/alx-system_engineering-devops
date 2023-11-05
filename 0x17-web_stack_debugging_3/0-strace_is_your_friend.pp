928b254a54a2d952e0a8f9e9de628d0c1bd48590
BradleyGilden <bradleygilden@gmail.com>
2023-11-05 18:23:44 +0200

    find apache error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes an Apache 500 error in a misconfigured server
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+# Web Stack Debugging #3
+
+we are tasked to create a manifest that fixes a status code 500 error coming from an Apache server. `strace` is used to locate the faults
