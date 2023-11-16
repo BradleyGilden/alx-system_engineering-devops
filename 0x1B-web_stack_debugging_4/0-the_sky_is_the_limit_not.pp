f91ed22ca74476aa30353c6ac8081fad1bb621c2
BradleyGilden <bradleygilden@gmail.com>
2023-11-16 09:47:21 +0200

    changed puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
-# Takes care of Too many open files error by increasing traffic.
+# Increases the amount of traffic an Nginx server can handle
 
-# Increase the ULIMIT of the default configuration
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  command => 'sed -i s/15/4096/ /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
 
-# Reload configurations
+#restart Nginx
+
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
